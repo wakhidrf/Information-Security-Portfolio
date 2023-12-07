9140f5feff2cf84d854e2f1b79e852c495191971
wakhidrf <wakhidrahmadfaisal@gmail.com>
2023-12-07 12:28:22 +0700

    Update

--- Information-Security-with-HelmetJS/boilerplate-bcrypt/server.js
+++ Information-Security-with-HelmetJS/boilerplate-bcrypt/server.js
@@ -4,50 +4,27 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
 fccTesting(app);
+const bcrypt = require('bcrypt')
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash)
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res)
+      });
+  });
 
 //END_ASYNC
 
 //START_SYNC
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+console.log(hash)
 
-
-
+const result = bcrypt.compareSync(myPlaintextPassword, hash)
+console.log(result)
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, () => {});
